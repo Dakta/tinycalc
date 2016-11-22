84e86b4d94dabc6ec043f7899c6e09c5a45436b8
Dakota Schneider <dakota@hackthetruth.org>
2016-11-21 21:37:26 -0800

    Initial Commit

---
+++ tinycalc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tinycalc
+//
+//  Created by Dakota Schneider on 11/21/16.
+//  Copyright © 2016 Code From Above. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
